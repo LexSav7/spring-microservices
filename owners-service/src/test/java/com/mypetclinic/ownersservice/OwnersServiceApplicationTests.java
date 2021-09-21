661d06d937e6c757d73f9a9690c79f0b45861082
asdf <asdf>
2021-09-21 14:08:00 +0300

    init commit

---
+++ owners-service/src/test/java/com/mypetclinic/ownersservice/OwnersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypetclinic.ownersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
